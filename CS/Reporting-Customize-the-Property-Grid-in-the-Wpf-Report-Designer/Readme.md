cca388c112eed0668a1cdb76fd102bd76be7cd97
Andrey Lepikhov (auto) <andrey.lepikhov@devexpress.com>
2022-12-10 13:55:40 +0400

    auto sync for version 2022.2

--- CS/Reporting-Customize-the-Property-Grid-in-the-Wpf-Report-Designer/Readme.md
+++
@@ -1,5 +0,0 @@
-# How to customize the Properties Window in the WPF End-User Report Designer
-
-This example demonstrates how to move a property to a custom tab in the Report Designer's [Properties Window](https://docs.devexpress.com/XtraReports/114799). The example also shows how to provide custom tab icons. 
-
-Refer to the [Customize the Properties Window in the Report Designer](https://docs.devexpress.com/XtraReports/400942) topic for more information.
---
+++ VB/Tests/Tests.vb
@@ -0,0 +1,29 @@
+﻿Imports System.Linq
+Imports System.Threading
+Imports System.Windows.Threading
+Imports DevExpress.Mvvm.UI
+Imports DevExpress.Xpf.Core
+Imports DevExpress.Xpf.PropertyGrid
+Imports DevExpress.Xpf.Reports.UserDesigner.PropertyGrid
+Imports NUnit.Framework
+
+Namespace CustomizePropertyGridCategories
+	<TestFixture, Apartment(ApartmentState.STA)>
+	Public Class Tests
+		<Test>
+		Public Sub CustomCategoryIsApplied()
+			Dim window = New MainWindow()
+			window.Show()
+			DispatcherHelper.DoEvents(DispatcherPriority.ApplicationIdle, 7)
+			DispatcherHelper.UpdateLayoutAndDoEvents(window, DispatcherPriority.ApplicationIdle)
+			Dim propertyGrid = LayoutTreeHelper.GetVisualChildren(window).OfType(Of ReportDesignerPropertyGrid)().Single()
+			Dim propertyGridView = propertyGrid.View
+			Dim categoriesSource = propertyGridView.CategoriesSource.Cast(Of RowData)().ToList()
+
+			Assert.That(categoriesSource.Count, [Is].EqualTo(7))
+			Dim myTabCategory = categoriesSource(5)
+			Assert.That(myTabCategory.IsCategory)
+			Assert.That(myTabCategory.Header, [Is].EqualTo("My tab"))
+		End Sub
+	End Class
+End Namespace
